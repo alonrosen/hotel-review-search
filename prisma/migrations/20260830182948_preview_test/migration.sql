cceb83a29acc05aa332175086d7e6098b49056d2
Alon <alonon2002@yahoo.com>
2026-08-30 21:59:33 +0300

    chore: revert test migrations and clean up

--- prisma/migrations/20260830182948_preview_test/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "preview_test" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "preview_test_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20260830184821_preview_test_two/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "preview_test_two" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-
-    CONSTRAINT "preview_test_two_pkey" PRIMARY KEY ("id")
-);
