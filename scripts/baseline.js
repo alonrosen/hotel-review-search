2a0338583425174d8eb2694e2a61e84576ae245a
Alon <alonon2002@yahoo.com>
2026-08-30 21:27:28 +0300

    chore: remove baseline script

--- scripts/baseline.js
+++
@@ -1,8 +0,0 @@
-const { execSync } = require('child_process');
-
-try {
-  console.log('Baselining database with 20260830182136_init...');
-  execSync('npx prisma migrate resolve --applied 20260830182136_init', { stdio: 'inherit' });
-} catch (error) {
-  console.log('Migration already applied or resolve failed. Continuing...');
-}
